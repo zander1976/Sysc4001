e211b8f6a79b04f66d962790b9299f4b182c9e1f
Ben Woodhead <benwoodhead@hotmail.com>
2023-11-06 11:42:40 -0500

    added ignore files

--- pcb.h
+++ pcb.h
@@ -48,41 +48,6 @@ void _pbc_admit_job(pcb_t* pcb, job_t* job) {
     pcb->io_duration = job->io_duration;
 
     pcb->remaining_io_cycles = job->io_duration;
-
-    // Create a process control block
-    // new_process->pid = accepted_job->pid;
-    // new_process->program_counter = 0;
-    // new_process->flags = 0;
-    // new_process->total_cpu = accepted_job->total_cpu;
-    // new_process->text_start_address = main_memory->count;
-    // new_process->remaining_io_cycles = 0;
-
-    // //_pcb_print(new_process);
-    // //_queue_push(process_queue, new_process);
-
-    // // The start data is after JOB and RUN
-    // //size_t data_start = new_process->text_start_address + accepted_job->total_cpu + 2;
-    // size_t data_start = accepted_job->total_cpu + 3;
-
-    // // Kernel Mode
-    // opcode_t header[] = {JOB|data_start, RUN};
-    // _main_memory_append(main_memory, header, 2);
-
-    // // Setup the main program
-    // for(unsigned int i = 1; i < accepted_job->total_cpu+1; i++) {
-    //     if ((i % accepted_job->io_frequency) == 0 ) {
-    //         opcode_t code[] = {READ | data_start+1};
-    //          _main_memory_append(main_memory, code, 1);
-    //     } else {
-    //         opcode_t code[] = {NOP};
-    //         _main_memory_append(main_memory, code, 1);
-    //     }
-    // }
-    // opcode_t footer[] = {END|data_start};
-    // _main_memory_append(main_memory, footer, 1);
-
-    // unsigned int data[] = {accepted_job->pid, accepted_job->io_duration};
-    // _main_memory_append(main_memory, data, 2);
 }
 
 void _pcb_print(pcb_t* pcb) {
